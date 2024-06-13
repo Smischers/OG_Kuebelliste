10fadc56d0b27c12fc99f062772884bb84c9a7cf
mihael brsanovic <mihael.brsanovic@kwp.at>
2024-06-13 12:29:18 +0200

    Added Vue Component

---
+++ server/files/components.js
@@ -0,0 +1,14 @@
+Vue.component('list-card', {
+  props: ['name', 'imgsrc'],
+  template: `<a href="google.com">
+                  <div class="card hoverable" style="width: 170px; height: 220px;">
+                    <div class="card-image">
+                      <img :src=imgsrc style="width:170px; height:150px;">
+                    </div>
+                    <div class="card-content">
+                      <span class="card-title center"
+                        style="font-size: 18.5px !important; color: black !important;"><b>{{ name }}</b></span>
+                    </div>
+                  </div>
+                </a>`
+});

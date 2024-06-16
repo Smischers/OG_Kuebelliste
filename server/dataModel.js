48895d383dc103e7e1ae0cc470e10677fc86b4b9
Koeko314 <konstantin.koefler@stud.fh-campuswien.ac.at>
2024-06-16 20:09:04 +0200

    Changed GET Endpoints to fit with FrontEnd

--- server/dataModel.js
+++ server/dataModel.js
@@ -1,71 +1,82 @@
 main = {
     List1: {
-        Name: "Name1",
-        Icon: "Link to Pic",
-        Header1: {
-            Name: "To Do",
-            Entry1: {
-                Title: "A Title",
-                Description: "A Descripton",
-                Picture: "A Picture-Link",
-                EndDate: 12 - 12 - 24
-            },
-            Entry2: {
-                Title: "A Title",
-                Description: "A Descripton",
-                Picture: "A Picture-Link",
-                EndDate: 12 - 12 - 24
-            }
-        },
-        Header2: {
-            Name: "Done",
-            Entry1: {
-                Title: "A Title",
-                Description: "A Descripton",
-                Picture: "A Picture-Link",
-                EndDate: 12 - 12 - 24
-            },
-            Entry2: {
-                Title: "A Title",
-                Description: "A Descripton",
-                Picture: "A Picture-Link",
-                EndDate: 12 - 12 - 24
-            }
-        }
+        name: "ESO Dungeons", 
+        picture: "https://assetsio.gnwcdn.com/elder-scrolls-online-logo.jpg?width=1200&height=1200&fit=crop&quality=100&format=png&enable=upscale&auto=webp",
+        headers: [
+            {
+                "To-Do": [
+                  {
+                    name: "Coral Aerie",
+                    rating: "0 / 6",
+                    description: "The Coral Aerie dungeon was added to the game with Update 33 Ascending Tide DLC. It features both Normal and Veteran modes, as well as Hardmodes for each boss and secret bosses. Players can either use the Group & Activity Finder in order to enter the dungeon as a 4 player group, or select it from the northern Summerset map. Coral Aerie has 7 bosses: 3 main bosses with each having a hardmode, 4 secret bosses and trashmob packs.",
+                    deadline: "30.03.2024",
+                    calendar: "yes",
+                  },
+                  {
+                    name: "Spindelclutch II",
+                    rating: "3 / 6",
+                    description: "The storyline continues from the events of Spindleclutch I.",
+                    deadline: "25.03.2024",
+                    calendar: "yes",
+                  }]
+              },
+              {
+                "Done": [
+                  {
+                    name: "Spindelclutch I",
+                    rating: "6 / 6",
+                    description: "The cave is infested with spiders, although something more sinister lurks within. Praxin Douare wants your help clearing the cave of spiders and other possible threats. In Spindleclutch I, a constant, pervasive whispering sound is present throughout the dungeon, and only gets louder as you descend through the caves.",
+                    deadline: "16.03.2024",
+                    calendar: "no",
+                  }]
+              },
+              {
+                "Test": []
+              },
+        ]
     },
     List2: {
-        Name: "Name2",
-        Icon: "Link to Pic",
-        Header1: {
-            Name: "To Do",
-            Entry1: {
-                Title: "A Title",
-                Description: "A Descripton",
-                Picture: "A Picture-Link",
-                EndDate: 12 - 12 - 24
+        name: "AnimeList", picture: "https://image.spreadshirtmedia.net/image-server/v1/mp/products/T1459A839PA4459PT28D187144250W10000H4186/views/1,width=800,height=800,appearanceId=839,backgroundColor=F2F2F2/anime-logo-sticker.jpg",
+        headers: [
+            {
+                "To Do":
+                    [
+                        {
+                            name: "A Title",
+                            rating: "0/10",
+                            description: "A Descripton",
+                            deadline: "some date",
+                            calendar: "yes"
+                        },
+                        {
+                            name: "A Title",
+                            rating: "0/10",
+                            description: "A Descripton",
+                            deadline: "some date",
+                            calendar: "yes"
+                        }
+                    ]
             },
-            Entry2: {
-                Title: "A Title",
-                Description: "A Descripton",
-                Picture: "A Picture-Link",
-                EndDate: 12 - 12 - 24
+            {
+                "Done":
+                    [
+                        {
+                            name: "A Title",
+                            rating: "0/10",
+                            description: "A Descripton",
+                            deadline: "some date",
+                            calendar: "yes"
+                        },
+                        {
+                            name: "A Title",
+                            rating: "0/10",
+                            description: "A Descripton",
+                            deadline: "some date",
+                            calendar: "yes"
+                        }
+                    ]
             }
-        },
-        Header2: {
-            Name: "Done",
-            Entry1: {
-                Title: "A Title",
-                Description: "A Descripton",
-                Picture: "A Picture-Link",
-                EndDate: 12 - 12 - 24
-            },
-            Entry2: {
-                Title: "A Title",
-                Description: "A Descripton",
-                Picture: "A Picture-Link",
-                EndDate: 12 - 12 - 24
-            }
-        }
+        ]
     }
 }
 
--- server/files/Endpoints.js
+++ server/files/Endpoints.js
@@ -7,7 +7,7 @@ exports.getList = function (req, res) {
 exports.getListByName = function (req, res) {
     let search = Object.keys(data).find(ele => ele === req.params.listname)
     if (search != null) {
-        res.send(data[search])
+        res.send(data[search].headers)
     } else {
         res.sendStatus(404)
     }
@@ -17,7 +17,7 @@ exports.getListNamesIcons = function (req, res) {
     let keys = Object.keys(data)
     var nameIcons = new Array
     for (let i = 0; i < keys.length; i++) {
-        nameIcons[i] = { Name: data[keys[i]].Name, Icon: data[keys[i]].Icon }
+        nameIcons[i] = { Name: data[keys[i]].name, Icon: data[keys[i]].picture }
     }
     res.send(nameIcons)
 }

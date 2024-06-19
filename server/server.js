029190778c4f7a89c9323b3a4b965240776bd77a
Smischers <julianschmitt1995@gmail.com>
2024-06-19 12:10:20 +0200

    Add example to get Request

--- server/server.js
+++ server/server.js
@@ -69,6 +69,7 @@ app.use(express.static(path.join(__dirname, 'files')));
  *       - in: path
  *         name: listName
  *         required: true
+ *         example: ESO Dungeons
  *         schema:
  *           type: string
  *         description: Enter the list you want to have returned here
